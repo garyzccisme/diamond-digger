190061849e846a80bcf15d5a8f94dde2ab636ae0
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-04-20 23:21:09 -0400

    add docstring

--- preprocessing/imputer.py
+++ preprocessing/imputer.py
@@ -5,22 +5,49 @@ from sklearn.exceptions import NotFittedError
 
 
 class DateImputer(BaseEstimator, TransformerMixin):
+    """
+    Imputer for date type column.
+    Default taking all three date columns: 'First Available Date', 'Last Available Date', 'Delivery Date'
+    # TODO: add more imputation strategies.
+    """
     def __init__(self, first_available_date='earliest', last_available_date='latest', deliver_date='latest'):
+        """
+        Choose imputation strategy for columns.
+
+        Args:
+            first_available_date: Default 'earliest', impute with earliest date.
+            last_available_date: Default 'latest', impute with latest date.
+            deliver_date: Default 'latest', impute with latest date.
+        """
         self.fad = first_available_date
         self.lad = last_available_date
         self.dd = deliver_date
+
+        # self.na_index stores all null index
         self.na_index = None
         self.imputed_values = {}
+
+        # Fit flag to prevent transform before fit
         self.is_fitted = False
 
-    def _validate_input(self, X):
+    def _validate_input(self, X: pd.DataFrame):
+        """
+        Detect null value for input X then store index into self.na_index.
+        """
         self.na_index = {
             'fad': X[X['First Available Date'].isna()].index,
             'lad': X[X['Last Available Date'].isna()].index,
             'dd': X[X['Delivery Date'].isna()].index,
         }
 
-    def fit(self, X, y=None):
+    def fit(self, X: pd.DataFrame, y=None):
+        """
+        Generate new dataframe for imputation then store into self.imputed_values.
+
+        Args:
+            X: Input data. Only support for pd.DataFrame.
+
+        """
         self._validate_input(X)
         if self.fad == 'earliest':
             self.imputed_values['fad'] = X.loc[self.na_index['fad'], 'First Available Date'].apply(
@@ -34,10 +61,21 @@ class DateImputer(BaseEstimator, TransformerMixin):
             self.imputed_values['dd'] = X.loc[self.na_index['dd'], 'Delivery Date'].apply(
                 lambda x: X['Delivery Date'].dropna().max()
             )
+
+        # Change fit flag
         self.is_fitted = True
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
+        """
+        Impute input X's null values with self.imputed_values. Must run after fit().
+
+        Args:
+            X: Input data. Only support for pd.DataFrame.
+
+        Returns: Imputed DataFrame
+
+        """
         if not self.is_fitted:
             raise NotFittedError("This {} instance is not fitted yet. Call 'fit' with appropriate arguments "
                                  "before using this estimator.".format(type(self).__name__))
--- preprocessing/transformer.py
+++ preprocessing/transformer.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -5,18 +7,29 @@ from sklearn.preprocessing import LabelEncoder
 
 
 class ColumnSelector(BaseEstimator, TransformerMixin):
-    def __init__(self, col_name):
+    """
+    Transformer to filter columns, split input dataset into multiple pre-process pipeline.
+    """
+    def __init__(self, col_name: List):
         self.col_name = col_name
 
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X: pd.DataFrame, y=None):
         return X[self.col_name]
 
 
 class DateSplitTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, date_type, use_dates=None):
+    """
+    Transformer to break single date column into numerical columns by Year, Month, Day, etc.
+    """
+    def __init__(self, date_type: str, use_dates: List= None):
+        """
+        Args:
+            date_type: String, must be one of ['First Available Date', 'Last Available Date', 'Delivery Date'].
+            use_dates: List, default ['Year', 'Month', 'Day'].
+        """
         self._date_type = date_type
         if use_dates is None:
             use_dates = ['Year', 'Month', 'Day']
@@ -25,7 +38,7 @@ class DateSplitTransformer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X: pd.DataFrame, y=None):
         split_date = pd.DataFrame(columns=['{} {}'.format(self._date_type, x) for x in self._use_dates])
         for spec in self._use_dates:
             exec("split_date['{} {}'] = X['{}'].apply(lambda x: x.{})".format(
@@ -34,7 +47,18 @@ class DateSplitTransformer(BaseEstimator, TransformerMixin):
 
 
 class DateDeltaTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, delta_type, former_date=None, later_date=None):
+    """
+    Transformer to calculate days difference between two given dates.
+    """
+    def __init__(self, delta_type: str, former_date: str = None, later_date: str = None):
+        """
+        Args:
+            delta_type: String, must be one of ['deliver_days', 'in_stock_days', 'customized'].
+            former_date: if date_type is 'customized',
+                then must choose one of ['First Available Date', 'Last Available Date', 'Delivery Date'].
+            later_date: if date_type is 'customized',
+                then must choose one of ['First Available Date', 'Last Available Date', 'Delivery Date'].
+        """
         self.delta_type = delta_type
         self._former_date = former_date
         self._later_date = later_date
@@ -42,7 +66,7 @@ class DateDeltaTransformer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X: pd.DataFrame, y=None):
         if self.delta_type == 'deliver_days':
             delta = (X['Delivery Date'] - X['Last Available Date']).apply(lambda x: x.days)
         elif self.delta_type == 'in_stock_days':
@@ -51,4 +75,5 @@ class DateDeltaTransformer(BaseEstimator, TransformerMixin):
             delta = (X[self._later_date] - X[self._former_date]).apply(lambda x: x.days)
         else:
             raise ValueError("Invalid input")
+        # Reshape 1-D array to 2-D array so that can be merged in FeatureUnion() with other features.
         return delta.values.reshape(-1, 1)
