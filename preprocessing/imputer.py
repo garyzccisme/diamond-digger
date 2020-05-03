d39bc814c5e2346a1bc78e5a894ed45e292a02cd
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-05-02 23:08:04 -0400

    refactor date imputer

--- preprocessing/imputer.py
+++ preprocessing/imputer.py
@@ -30,16 +30,6 @@ class DateImputer(BaseEstimator, TransformerMixin):
         # Fit flag to prevent transform before fit
         self.is_fitted = False
 
-    def _validate_input(self, X: pd.DataFrame):
-        """
-        Detect null value for input X then store index into self.na_index.
-        """
-        self.na_index = {
-            'fad': X[X['First Available Date'].isna()].index,
-            'lad': X[X['Last Available Date'].isna()].index,
-            'dd': X[X['Delivery Date'].isna()].index,
-        }
-
     def fit(self, X: pd.DataFrame, y=None):
         """
         Generate new dataframe for imputation then store into self.imputed_values.
@@ -48,19 +38,12 @@ class DateImputer(BaseEstimator, TransformerMixin):
             X: Input data. Only support for pd.DataFrame.
 
         """
-        self._validate_input(X)
         if self.fad == 'earliest':
-            self.imputed_values['fad'] = X.loc[self.na_index['fad'], 'First Available Date'].apply(
-                lambda x: X['First Available Date'].dropna().min()
-            )
+            self.imputed_values['First Available Date'] = X['First Available Date'].dropna().min()
         if self.lad == 'latest':
-            self.imputed_values['lad'] = X.loc[self.na_index['lad'], 'Last Available Date'].apply(
-                lambda x: X['Last Available Date'].dropna().max()
-            )
+            self.imputed_values['Last Available Date'] = X['Last Available Date'].dropna().max()
         if self.dd == 'latest':
-            self.imputed_values['dd'] = X.loc[self.na_index['dd'], 'Delivery Date'].apply(
-                lambda x: X['Delivery Date'].dropna().max()
-            )
+            self.imputed_values['Delivery Date'] = X['Delivery Date'].dropna().max()
 
         # Change fit flag
         self.is_fitted = True
@@ -80,7 +63,5 @@ class DateImputer(BaseEstimator, TransformerMixin):
             raise NotFittedError("This {} instance is not fitted yet. Call 'fit' with appropriate arguments "
                                  "before using this estimator.".format(type(self).__name__))
 
-        X.loc[self.na_index['fad'], 'First Available Date'] = self.imputed_values['fad']
-        X.loc[self.na_index['lad'], 'Last Available Date'] = self.imputed_values['lad']
-        X.loc[self.na_index['dd'], 'Delivery Date'] = self.imputed_values['dd']
+        X.fillna(value=self.imputed_values, inplace=True)
         return X
