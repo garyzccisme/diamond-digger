f85ab9eba2909cc8b795bb39cd33b87ac6a9f864
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-05-05 15:33:53 -0400

    add some utils

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -4,52 +4,63 @@ from sklearn.base import BaseEstimator
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, OrdinalEncoder, StandardScaler
 
 from preprocessing.imputer import DateImputer
 from preprocessing.transformer import ColumnSelector, DateDeltaTransformer, DateSplitTransformer
 
 
-def generate_signal_feature_union(signal_list, signal_hyper_dict_prefix="signals"):
-    # ToDo: add example!
-    """Generate feature union estimator and hyper parameter dict from signal config list.
-    :param list(dict) signal_list: List containing a dict referring to each transformer to add to
-        the feature union estimator. Each dict should contain three keys -- 'desc' (optional), 'func',
-        and 'tuning_params'. 'Desc' is a user-defined name of the transformer (optional), 'func' is a valid
-        transformer function, and 'tuning_params' is a ``dict`` of parameters to pass to the transformer. If there
-        are none, set to empty dict.
-    :param str signal_hyper_dict_prefix: String to prepend to the signal hyper parameters, in order to use with
-        `Pipeline` class. This string should match what the feature union is named in the pipeline. Defaults to
-        `signals`.
-    :returns: tuple: The initialized feature union and associated hyper-parameter dict.
+def generate_tuning_dict(tune_params=None):
     """
-    signal_pipe_list = []
-    signal_tuning_dict = {}
-    signal_count_dict = {}
+    Helper function to generate hyper-parameters dict for cv pipeline. Simply reconstruct parameters name.
+    Args:
+        tune_params: Dict, {'prefix': {'param_name': distribution(List)}}
 
-    for signal in signal_list:
-        if "desc" in signal.keys():
-            signal_desc = signal["desc"]
-        else:
-            class_name = signal["func"].__class__.__name__
-            if class_name not in signal_count_dict.keys():
-                signal_count_dict[class_name] = 0
-            else:
-                signal_count_dict[class_name] += 1
-            signal_desc = '{}_{}'.format(class_name, signal_count_dict[class_name])
+    Returns: Dict, {'prefix__param_name': distribution(List)}}
 
-        signal_pipe_list.append((signal_desc, signal["func"]))
-
-        for param_name, param_values in signal["tuning_params"].items():
-            signal_tuning_dict.update(
-                {'{}__{}__{}'.format(signal_hyper_dict_prefix, signal_desc, param_name): param_values}
-            )
-    signal_feature_union = FeatureUnion(signal_pipe_list)
-    return signal_feature_union, signal_tuning_dict
+    """
+    tuning_dict = {}
+    if tune_params is not None:
+        for prefix, params in tune_params.items():
+            for name, distribution in params.items():
+                tuning_dict['{}__{}'.format(prefix, name)] = distribution
+    return tuning_dict
 
 
-def get_cat_preprocessor(columns, encoder_type, tune_params=None):
+def generate_feature_union(transformer_dict_list):
+    """
+    Helper function to generate FeatureUnion given by a transformer_list.
+    Args:
+        transformer_dict_list: List, List containing a Dict referring to each transformer to add to the feature union
+        estimator. Each Dict should contain three keys:
+            {
+                'prefix'(optional): String, The prefix name of the transformer(pipeline), if None then generate default.
+                'transformer': Transformer or Pipeline, which's supposed to add into FeatureUnion.
+                'tuning_params'(optional): Dict, {'param_name': distribution(list)}. Hyper-params prepared to tuned.
+            }
 
+    Returns: FeatureUnion, Tuning Parameters Dict
+
+    """
+    transformer_list = []
+    transformer_count = {}
+    tune_params = {}
+    for transformer_dict in transformer_dict_list:
+        if transformer_dict.get('transformer') is None:
+            raise ValueError("Please make sure transformer is given.")
+        if transformer_dict.get('prefix') is None:
+            transformer_name = transformer_dict['transformer'].__class__.__name__
+            transformer_count['transformer_name'] = transformer_count.get('transformer_name', 0) + 1
+            transformer_dict['prefix'] = transformer_name + '_{}'.format(transformer_count['transformer_name'])
+        transformer_list.append((transformer_dict['prefix'], transformer_dict['transformer']))
+        if transformer_dict.get('tuning_params'):
+            tune_params[transformer_dict['prefix']] = transformer_dict['tuning_params']
+    tuning_dict = generate_tuning_dict(tune_params)
+    feature_union = FeatureUnion(transformer_list=transformer_list)
+    return feature_union, tuning_dict
+
+
+def generate_cat_preprocessor(columns, imputer_strategy='most_frequent', encoder_type='Ordinal', tune_params=None):
     if encoder_type == 'Ordinal':
         encoder = OrdinalEncoder()
     elif encoder_type == 'OneHot':
@@ -59,16 +70,29 @@ def get_cat_preprocessor(columns, encoder_type, tune_params=None):
 
     cat_preprocessor = Pipeline([
         ('selector', ColumnSelector(columns)),
-        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('imputer', SimpleImputer(strategy=imputer_strategy)),
         ('encoder', encoder),
     ])
+    feature_name = columns
+    tuning_dict = generate_tuning_dict(tune_params)
+    return cat_preprocessor, feature_name, tuning_dict
 
-    tuning_dict = {}
-    if tune_params is not None:
-        for prefix, params in tune_params.items():
-            for name, distribution in params.items():
-                tuning_dict['{}__{}'.format(prefix, name)] = distribution
 
-    return cat_preprocessor, tuning_dict
+def generate_num_preprocessor(columns, imputer_strategy='median', scaler_type='Standard', tune_params=None):
+    if scaler_type == 'Standard':
+        scaler = StandardScaler()
+    elif scaler_type == 'MinMax':
+        scaler = MinMaxScaler()
+    else:
+        raise ValueError("Invalid scaler_type, should be one of ['Standard', 'MinMax']")
+
+    num_preprocessor = Pipeline([
+        ('selector', ColumnSelector(columns)),
+        ('imputer', SimpleImputer(strategy=imputer_strategy)),
+        ('scaler', scaler)
+    ])
+    feature_name = columns
+    tuning_dict = generate_tuning_dict(tune_params)
+    return num_preprocessor, feature_name, tuning_dict
 
 
